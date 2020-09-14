89f0beb1c631cead91b618bccbf896fb59c51b3c
Pijus <user@users-MBP.lan>
2020-09-14 19:42:01 +0300

    Updates

---
+++ precache-manifest.fa97fe8421febc2e1f27b87f30441d1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0710c839cc0f4b520a0d246c395efe21",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "73a7348488fec8b4ca95",
+    "url": "/weather-app/static/css/main.be116681.chunk.css"
+  },
+  {
+    "revision": "b8b4acc956a468295796",
+    "url": "/weather-app/static/js/2.8c2ae7e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.8c2ae7e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73a7348488fec8b4ca95",
+    "url": "/weather-app/static/js/main.b530d46f.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
